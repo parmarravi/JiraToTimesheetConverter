06069476c86379653925542def253e4b571ebbef
Ravi Parmar <parmarravi950@gmail.com>
2025-09-07 01:41:58 +0530

    debug removed

--- app.py
+++ app.py
@@ -1766,4 +1766,4 @@ def download_report(report_type):
     # )
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5005)
+    app.run(debug=False, host='0.0.0.0', port=8080)
